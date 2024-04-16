f3a3ca7eb9cc487acd1ef96edb8fa90d67861725
Tomasz <bleniog@interia.pl>
2024-04-16 14:49:13 +0200

    GoBikeRide: Modyfied navbar styles

--- src/ui/organisms/Navigation/Nav.tsx
+++ src/ui/organisms/Navigation/Nav.tsx
@@ -33,7 +33,7 @@ const Nav = () => {
     <nav
       className={` flex h-screen ${isSmallScreen ? 'w-20 p-3' : 'w-72 p-6'}  z-20 flex-col items-start justify-between border-r-[1px]`}
     >
-      <ul className='flex flex-col items-center  justify-center gap-2 space-y-4'>
+      <ul className='flex  flex-col items-center  justify-center gap-2 space-y-4'>
         <p
           className={`flex items-center gap-2 text-[28px] ${isSmallScreen && 'self-start'}`}
         >
@@ -44,7 +44,7 @@ const Nav = () => {
         </p>
         {menuList.map((item) => (
           <li
-            className={`flex w-full  justify-start text-nowrap rounded-lg border-[1px] border-gray-300 shadow-md backdrop-blur-xl
+            className={`flex w-[95%]  justify-start text-nowrap rounded-lg border-[1px] border-b-8 border-r-8 border-[#5F286B] shadow-md backdrop-blur-xl duration-300 hover:translate-x-[4px] hover:translate-y-[4px] hover:border-b-4 hover:border-r-4
               ${router.includes(item.link) ? selectedItem : 'hover:bg-slate-400'} `}
             key={item.title}
           >
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,9 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      boxShadow: {
+        custom: '10px 10px 10px black'
       }
     }
   },
