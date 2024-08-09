124cf2bff63bd434e4dffd7c7a46b9e18fc03f81
Tomasz <bleniog@interia.pl>
2024-08-09 03:11:23 +0200

    Add-frequently-asked-questions

---
+++ src/sections/premium/FrequentlyAskedQuestions/FrequentlyAskedQuestions.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+
+const FrequentlyAskedQuestions = () => {
+  return (
+    <div className='mt-10 flex flex-col items-center gap-4'>
+      <h2 className='text-[48px] font-bold'>Często zadawane pytania</h2>
+      <p className='text-center text-gray-500'>
+        Jeśli nie możesz znaleść tutaj odpowiedzi, sprawdz nasze
+        <span className='text-wite mx-1 font-bold text-secoundSea'>
+          forum pomocy
+        </span>{' '}
+        , lub napisz do nas
+        <span className='mx-1 font-bold text-secoundSea shadow-sm'>
+          supportGoBikRide@.gmail.com
+        </span>
+      </p>
+      <div className='flex flex-col gap-4'>
+        <Accordion className=' font-bold'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls='panel1-content'
+            id='panel1-header'
+          >
+            <Typography>Które opcje płacenia są dostępne ?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls='panel2-content'
+            id='panel2-header'
+          >
+            <Typography>Co się stanie jak już osiągne limit trzech rowerów</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion disabled>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls='panel3-content'
+            id='panel3-header'
+          >
+            <Typography>Disabled Accordion</Typography>
+          </AccordionSummary>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default FrequentlyAskedQuestions;
--- src/sections/premium/PremiumView.tsx
+++ src/sections/premium/PremiumView.tsx
@@ -1,15 +1,16 @@
 'use client';
 
+import FrequentlyAskedQuestions from './FrequentlyAskedQuestions/FrequentlyAskedQuestions';
 import PricingCard from './PricingCard/PricingCard';
 import { pricingList } from './pricingList';
 
 const PremiumView = () => {
   return (
-    <section className='flex flex-col items-center space-y-10 p-4'>
-      <h2 className='max-w-[700px] text-center text-[46px] font-bold'>
+    <section className='flex flex-col items-center w-full space-y-10 p-4 '>
+      <h2 className=' text-center text-[46px] font-bold'>
         Korzystaj Ze Wszystkich Funkcjonalności!
       </h2>
-      <p className='max-w-[700px] text-balance text-center text-gray-500'>
+      <p className=' text-balance text-center text-gray-500'>
         Zobacz co obejmuje pakiet premium. Dzięki pakietowi premium zyskujesz
         dostęp do wielu nowych i ekscytujących funkcji, które stale rozwijamy.
         Ciesz się innowacjami i dodatkowymi udogodnieniami, które uczynią Twoje
@@ -28,6 +29,7 @@ const PremiumView = () => {
           price={pricingList.premium.price}
         />
       </div>
+      <FrequentlyAskedQuestions />
     </section>
   );
 };
