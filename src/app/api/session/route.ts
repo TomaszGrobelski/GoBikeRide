5431ff101e07ccf26e81934a8e66927e9798d2ec
Tomasz <bleniog@interia.pl>
2024-10-30 11:54:24 +0100

    GoBikeRide: Testing-deploy-vercel#7

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -7,15 +7,18 @@ import { createServerClient } from '@/lib/supabase/server-client';
 export async function GET(req: NextRequest) {
   const supabase = createServerClient(req);
 
+  // Dodaj nagłówek CORS
+  const response = NextResponse.json({}); // Zwróć pusty obiekt lub cokolwiek, co chcesz
+  response.headers.set(
+    'Access-Control-Allow-Origin',
+    'https://go-bike-ride-1eypllk8a-tomaszs-projects-594afce0.vercel.app',
+  ); // Dodaj konkretny origin
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Dozwolone metody
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Dozwolone nagłówki
+
   const {
     data: { user },
-    error
   } = await supabase.auth.getUser();
-
-  if (error) {
-    console.error('Error fetching user:', error);
-    return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
-  }
 
   if (!user) {
     return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
