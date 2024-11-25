8886ce95afa70989a48b9bb6443b8ab25476045e
Tomasz <bleniog@interia.pl>
2024-11-25 10:59:09 +0100

    GoBikeRide: Fixes-theme#4

--- src/loyout/DashboardLoyout/DashboardLoyout.tsx
+++ src/loyout/DashboardLoyout/DashboardLoyout.tsx
@@ -14,66 +14,64 @@ import { useTheme } from 'next-themes';
 import useIsSmallScreen from '@/hooks/use-IsSmallScreen';
 
 interface IDashboardLayout {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 const DashboardLoyout = ({ children }: IDashboardLayout) => {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
-  const router = usePathname();
-  const isSmallScreen = useIsSmallScreen();
-  const [isExpanded, setIsExpanded] = useState(!isSmallScreen);
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+    const router = usePathname();
+    const isSmallScreen = useIsSmallScreen();
+    const [isExpanded, setIsExpanded] = useState(!isSmallScreen);
 
-  const sectionNames = {
-    bike: 'Garaż',
-    'go-bike': 'Go Bike',
-    road: '',
-    hero: '',
-    posts: 'Posty',
-    users: 'Lista użytkowników',
-    contact: '',
-  };
+    const sectionNames = {
+        bike: 'Garaż',
+        'go-bike': 'Go Bike',
+        road: '',
+        hero: '',
+        posts: 'Posty',
+        users: 'Lista użytkowników',
+        contact: '',
+    };
 
-  const sectionName =
-    sectionNames[(router.split('/').pop() as keyof typeof sectionNames) || ''];
+    const sectionName = sectionNames[(router.split('/').pop() as keyof typeof sectionNames) || ''];
 
-  const dashboardBackGround = theme === 'light' ? light : dashboardBG;
+    const dashboardBackGround = theme === 'light' ? light : dashboardBG;
 
-  useEffect(() => {
-    setMounted(true);
-    const storedTheme = localStorage.getItem('theme');
-    if (storedTheme && storedTheme !== theme) {
-      setTheme(storedTheme);
+    useEffect(() => {
+        setMounted(true);
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme && storedTheme !== theme) {
+            setTheme(storedTheme);
+        }
+    }, [theme, setTheme]);
+
+    if (!mounted) {
+        return null;
     }
-  }, [theme, setTheme]);
 
-  if (!mounted) {
-    return null;
-  }
-
-  return (
-    <div
-      className={`relative flex min-h-full font-poppins ${theme === 'light' ? 'bg-lightBackground' : 'bg-[#030014]'} `}
-    >
-      <NavBar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
-      <UpperBar />
-      <Modal />
-      <motion.div
-        className={`relative z-0 flex min-h-full w-full flex-col  backdrop-blur-md`}
-        initial={{ marginLeft: isExpanded ? '5rem' : '16rem' }}
-        animate={{ marginLeft: isExpanded ? '5rem' : '16rem' }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
-      >
-        <Typography
-          variant='h1'
-          sx={{ fontSize: '2rem' }}
-          className='pl-6 pt-20 font-roboto font-bold text-black dark:text-slate-50'
-        >
-          {sectionName}
-        </Typography>
-        {children}
-      </motion.div>
-    </div>
-  );
+    //  ${theme === 'light' ? 'bg-lightBackground' : 'bg-[#030014]'}
+    return (
+        <div className={`relative flex min-h-full bg-lightBackground font-poppins`}>
+            <NavBar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+            <UpperBar />
+            <Modal />
+            <motion.div
+                className={`relative z-0 flex min-h-full w-full flex-col backdrop-blur-md`}
+                initial={{ marginLeft: isExpanded ? '5rem' : '16rem' }}
+                animate={{ marginLeft: isExpanded ? '5rem' : '16rem' }}
+                transition={{ duration: 0.5, ease: 'easeInOut' }}
+            >
+                <Typography
+                    variant='h1'
+                    sx={{ fontSize: '2rem' }}
+                    className='pl-6 pt-20 font-roboto font-bold text-black dark:text-slate-50'
+                >
+                    {sectionName}
+                </Typography>
+                {children}
+            </motion.div>
+        </div>
+    );
 };
 
 export default DashboardLoyout;
