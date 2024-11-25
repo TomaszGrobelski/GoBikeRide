7e6ed3b65c0fb0e45ba514555707fced1f9948a1
Tomasz <bleniog@interia.pl>
2024-11-25 10:55:47 +0100

    GoBikeRide: Fixes-theme#3

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,19 +8,19 @@ import { ThemeProvider } from 'next-themes';
 import queryClient from '@/lib/queryClient';
 
 type Props = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <DateLocalizationProvider>
-        {/* <ThemeProvider attribute='class' defaultTheme='system' enableSystem> */}
-          <DashboardLoyout>{children}</DashboardLoyout>
-        {/* </ThemeProvider> */}
-      </DateLocalizationProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <DateLocalizationProvider>
+                <ThemeProvider attribute='class' defaultTheme='light' enableSystem>
+                    <DashboardLoyout>{children}</DashboardLoyout>
+                </ThemeProvider>
+            </DateLocalizationProvider>
+        </QueryClientProvider>
+    );
 };
 
 export default Layout;
